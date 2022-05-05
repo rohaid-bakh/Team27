7fdce1faa59a6e54733da3edb4ac5e66dc513882
Rohaid <rohaidb1999@gmail.com>
2022-05-05 00:55:16 -0400

    Working on Health Bar System

--- Assets/Scripts/MC/Health.cs
+++ Assets/Scripts/MC/Health.cs
@@ -1,16 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class Health : MonoBehaviour
 {
     private int hpbar;
     public Stats stat;
+    [SerializeField]
+    private Slider healthbar;
     void Awake(){
         hpbar = stat.health;
+        healthbar.normalizedValue = 1;
     }
     public void TakeDamage(int damage){ // other classes call this
         hpbar -= damage;
+        healthbar.normalizedValue = 1f-((float)(stat.health-hpbar)/(float)stat.health);
         if(hpbar <= 0){
             Death();
         }
