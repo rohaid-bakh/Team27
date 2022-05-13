9708c2c5fae15067f9437da7c945da3d0348af18
ash-gamedev <adrouill1997@gmail.com>
2022-05-13 16:06:22 -0400

    Choose random victory/loss quote

--- Assets/Scripts/MC/Dialouge.cs
+++ Assets/Scripts/MC/Dialouge.cs
@@ -32,57 +32,80 @@ public class Dialouge : MonoBehaviour
     [SerializeField]
     private Image playerSprite;
 
+    System.Random random;
+
     private void Awake(){
         //Adds a listener to the main slider and invokes a method when the value changes.
         enemy.onValueChanged.AddListener(delegate {Victory(); });
         player.onValueChanged.AddListener(delegate {Loss(); });
+
+        random = new System.Random();
     }
 
-    public void Victory() // TODO: add randomizer to choose different quotes
+    public void Victory()
     {
      if(enemy.value == 0){
          // first disable player controls (we still want to see the player)
          Player.CanControlPlayer = false;
+                    
+         // victory quote (picks a random victory quote)
+         List<int> victoryQuoteIndexes = new List<int> { 0, 1 };
+         int randomVictoryQuoteIndex = victoryQuoteIndexes[random.Next(victoryQuoteIndexes.Count)];
+         string victoryQuote = playerDialoug.Dialouge[randomVictoryQuoteIndex]; // TODO: add randomizer to choose different quotes
 
-         textBox.SetActive(true);
-
-         playerSprite.sprite = playerDialoug.DialougeSprites[0]; // set up victory quote
-         TypeWriter.instance?.TypeWriteLine(playerDialoug.Dialouge[0], text); // set up victory dialouge
-        
+         // get current scene
          Scene scene = SceneManager.GetActiveScene();
 
          if(scene.name.Trim() == "MagogFight") // if it's the final boss, load outro dialog (TODO: replace with the real final scene name)
          {
-            StartCoroutine(StartVictorySceneDialog());
+            StartCoroutine(DialogAndStartVictorySceneDialog(victoryQuote));
          } // otherwise load next scene
          else 
          {
             int nextScene = scene.buildIndex + 1; // use this instead when actual level build index is finalized
-            StartCoroutine(SceneTransition(2)); // change the passed in when the actual level build index is finalized
+            StartCoroutine(DialogAndSceneTransition(victoryQuote, nextScene)); 
          }
          
      }  
     }
 
-    public void Loss(){ // TODO: add randomizer to choose different quotes
+    public void Loss(){ 
         if(player.value == 0){
             // first disable player controls (we still want to see the player)
             Player.CanControlPlayer = false;
 
-            textBox.SetActive(true);
-            playerSprite.sprite = playerDialoug.DialougeSprites[1];
-            TypeWriter.instance?.TypeWriteLine(playerDialoug.Dialouge[2], text);
-            StartCoroutine(SceneTransition(2));
+            // loss quote (picks a random loss quote)
+            List<int> lossQuoteIndexes = new List<int> { 2, 3 };
+            int randomLossQuoteIndex = lossQuoteIndexes[random.Next(lossQuoteIndexes.Count)];
+            string lossQuote = playerDialoug.Dialouge[randomLossQuoteIndex];
+
+            int loseScreenIndex = 3; // TODO: once build index is finalized, double check if this is okay
+            StartCoroutine(DialogAndSceneTransition(lossQuote, loseScreenIndex));
         }
     }
 
-    private IEnumerator StartVictorySceneDialog()
+    private IEnumerator DialogAndStartVictorySceneDialog(string victoryQuote)
     {
+        // no text to start
+        text.text = "";
+
+        // wait half a second before dialog text appears
+        yield return new WaitForSeconds(0.5f);
+
+        // show dialog box
+        textBox.SetActive(true);
+
+        // wait half a quarter second before dialog text appears
+        yield return new WaitForSeconds(0.25f);
+
+        playerSprite.sprite = playerDialoug.DialougeSprites[1];
+        TypeWriter.instance?.TypeWriteLine(victoryQuote, text);
+
         // wait for text to finish typing
         yield return new WaitUntil(() => TypeWriter.instance.isCurrentlyTyping == false);
 
-        // wait for one second
-        yield return new WaitForSeconds(1f);
+        // wait for 2 seconds
+        yield return new WaitForSeconds(2f);
 
         textBox.SetActive(false);
 
@@ -91,13 +114,27 @@ public class Dialouge : MonoBehaviour
         FindObjectOfType<OutroDialoug>().StartOutroDialog();
     }
 
-    private IEnumerator SceneTransition(int scene){
+    private IEnumerator DialogAndSceneTransition(string victoryOrLossQuote, int scene){
+        // no text to start
+        text.text = "";
+
+        // wait half a second before dialog text appears
+        yield return new WaitForSeconds(0.5f);
+
+        // show dialog box
+        textBox.SetActive(true);
+
+        // wait half a quarter second before dialog text appears
+        yield return new WaitForSeconds(0.25f);
+
+        playerSprite.sprite = playerDialoug.DialougeSprites[1];
+        TypeWriter.instance?.TypeWriteLine(victoryOrLossQuote, text);
 
         // wait for text to finish typing
         yield return new WaitUntil(() => TypeWriter.instance.isCurrentlyTyping == false);
 
-        // wait for one second
-        yield return new WaitForSeconds(1f);
+        // wait for 2 seconds
+        yield return new WaitForSeconds(2f);
 
         //transition
         sceneTransition.SetTrigger("startTransit");
